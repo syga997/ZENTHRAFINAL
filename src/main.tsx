8fb5c3d8326f5e86a14c4890b8880ac6ee477fdd
syga997 <syga997@gmail.com>
2025-06-09 17:53:20 +0000

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import './tailwind.css';
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Komponensek importálása
+import HeroSection from "./components/HeroSection";
+import BuySection from "./components/BuySection";
+import InteractiveStorytelling from "./components/InteractiveStorytelling";
+import Presale from "./components/Presale";
+import Roadmap from "./components/Roadmap";
+import TokenomicsChart from "./components/TokenomicsChart";
+import MultiChainSection from "./components/MultiChainSection";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <HeroSection />
+      <BuySection />
+      <InteractiveStorytelling />
+      <Presale />
+      <Roadmap />
+      <TokenomicsChart />
+      <MultiChainSection />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
